9500cb0869807a5cbb1e828b3c05c1b9eaf97acb
Piya Kaewket <41195318+IJerickoZ@users.noreply.github.com>
2021-02-09 00:16:20 +0700

    Create index.php

---
+++ Lab7/6/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-3 mt-2 mb-2">
+                <?php
+                    echo "
+                    <img style='width:95%;border-radius: 15px;' class='mt-2 mb-2 mr-2 ml-2' src='https://thermaltake.azureedge.net/pub/media/wysiwyg/key3/wallpaper/pc/TT_Chao2_1920x1200.jpg'>
+                    <img style='width:95%;border-radius: 15px;' class='mt-2 mb-2 mr-2 ml-2' src='https://i.redd.it/2vpqr3i54sy11.jpg'>
+                    <img style='width:95%;border-radius: 15px;' class='mt-2 mb-2 mr-2 ml-2' src='https://steamuserimages-a.akamaihd.net/ugc/940586530515504757/CDDE77CB810474E1C07B945E40AE4713141AFD76/'>
+                    <img style='width:95%;border-radius: 15px;' class='mt-2 mb-2 mr-2 ml-2' src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ab69e169-a09e-485b-97f3-d052c118f478/dbb93lt-84101a9b-669c-4f96-b5ac-5dffaee97392.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvYWI2OWUxNjktYTA5ZS00ODViLTk3ZjMtZDA1MmMxMThmNDc4XC9kYmI5M2x0LTg0MTAxYTliLTY2OWMtNGY5Ni1iNWFjLTVkZmZhZWU5NzM5Mi5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.KTM-JLBPBIbVHA27BxjO_12QjpQf6iXneJsP2Bt9W_U'>
+                    <img style='width:95%;border-radius: 15px;' class='mt-2 mb-2 mr-2 ml-2' src='https://s.isanook.com/hi/0/rp/r/w850/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL2hpLzAvdWQvMjkxLzE0NTk0MjcvdW50aXRsZWQtMy5qcGc=.jpg'>
+                    "
+                ?>
+            </div>
+            <div class="col-3">
+            <?php
+                    echo "
+                    <img style='width:95%;border-radius: 15px;' class='mt-2 mb-2 mr-2 ml-2' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTFXQOKrJTyqhCD0KsrXQf_VT5pkiujbQGxhQ&usqp=CAU'>
+                    <img style='width:95%;border-radius: 15px;' class='mt-2 mb-2 mr-2 ml-2' src='https://dslv9ilpbe7p1.cloudfront.net/THPURGrGd5hrhX9oXLnWEQ_store_banner_image.jpeg'>
+                    <img style='width:95%;border-radius: 15px;' class='mt-2 mb-2 mr-2 ml-2' src='https://i.pinimg.com/originals/e2/b8/2a/e2b82aded815e80351b929a77519adaa.jpg'>
+                    <img style='width:95%;border-radius: 15px;' class='mt-2 mb-2 mr-2 ml-2' src='https://media.idownloadblog.com/wp-content/uploads/2020/04/iPhone-SE-2020-wallpaper-PRODUCTRED-ispazio-wallpapers-central-idownloadblog.png'>
+                    <img style='width:95%;border-radius: 15px;' class='mt-2 mb-2 mr-2 ml-2' src='https://wallpapershome.com/images/pages/ico_v/22653.jpg'>
+                    "
+                ?>
+            </div>
+            <div class="col-3">
+            <?php
+                    echo "
+                    <img style='width:95%;border-radius: 15px;' class='mt-2 mb-2 mr-2 ml-2' src='https://i.pinimg.com/originals/aa/29/ff/aa29ff15d33248517ac1ad5132122db3.jpg'>
+                    <img style='width:95%;border-radius: 15px;' class='mt-2 mb-2 mr-2 ml-2' src='https://www.wallpapertip.com/wmimgs/37-373317_20-siamese-fighting-fish-hd-wallpapers-betta-fish.jpg'>
+                    <img style='width:95%;border-radius: 15px;' class='mt-2 mb-2 mr-2 ml-2' src='https://imgc.artprintimages.com/img/print/colourful-betta-fish-siamese-fighting-fish-in-movement-isolated-on-black-background_u-l-q19yt3r0.jpg?h=550&p=0&w=550&background=fbfbfb'>
+                    <img style='width:95%;border-radius: 15px;' class='mt-2 mb-2 mr-2 ml-2' src='https://i.pinimg.com/originals/83/14/aa/8314aad6e12116a0b1640d9334309689.jpg'>
+                    <img style='width:95%;border-radius: 15px;' class='mt-2 mb-2 mr-2 ml-2' src='https://obs.line-scdn.net/0h1oSSamgfbkBuKkR-PFgRF1R8bS9dRn1DChw_QzJEMHQRHiESBU8ldUIjMCVAGykeB00jIU8pdXFLEi0fVUwl/w644'>
+                    "
+                ?>
+            </div>
+            <div class="col-3">
+            <?php
+                    echo "
+                    <img style='width:95%;border-radius: 15px;' class='mt-2 mb-2 mr-2 ml-2' src='https://images.unsplash.com/photo-1507065255811-f3b9fe968f84?ixlib=rb-1.2.1&w=1000&q=80'>
+                    <img style='width:95%;border-radius: 15px;' class='mt-2 mb-2 mr-2 ml-2' src='https://1.bp.blogspot.com/-3Kx0SZvDAOw/XaolvnBctUI/AAAAAAAAAY8/Lnfd3rEQ2E8rGT6HWbigU6xyUjYOxL0xQCLcBGAsYHQ/w1200-h630-p-k-no-nu/Japanese%2BWallpaper.jpeg'>
+                    <img style='width:95%;border-radius: 15px;' class='mt-2 mb-2 mr-2 ml-2' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS2gK7c8ho6fxoYlFAvGapkiSkQnBXTEpz-aQ&usqp=CAU'>
+                    <img style='width:95%;border-radius: 15px;' class='mt-2 mb-2 mr-2 ml-2' src='https://www.designyourway.net/blog/wp-content/uploads/2018/01/3d-Desktop-Backgrounds.jpg'>
+                    <img style='width:95%;border-radius: 15px;' class='mt-2 mb-2 mr-2 ml-2' src='https://images.freecreatives.com/wp-content/uploads/2015/10/moon-images-at-night-dowload.jpg'>
+                    "
+                ?>
+            </div>
+        </div>
+    </div>
+</body>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+</html>
